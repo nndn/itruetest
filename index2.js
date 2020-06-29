8acc468fd349e30c29ed10f44762ad551a5ab44d
MAHE <mynameisgurunandan@gmail.com>
2020-06-29 10:19:43 +0530

    parallax

--- index2.js
+++ index2.js
@@ -4,15 +4,15 @@ state = 0
 $('.expand-icon').on('click',function(){
 
     if(state==0){
-        $('.nav-bar').css('display','grid');
-        $('.collapsible').css('display','grid');
+        $('.nav-bar').css('max-height','400px');
+        $('.collapsible').css('max-height','400px');
         state = 1;
         return;
     }
     
     if(state==1){
-        $('.nav-bar').css('display','none');
-        $('.collapsible').css('display','none');
+        $('.nav-bar').css('max-height','0px');
+        $('.collapsible').css('max-height','0px');
         state = 0;
         return;
     }

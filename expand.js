88f76e6281132a4ff2816ebb72151c90c8cddc97
MAHE <mynameisgurunandan@gmail.com>
2020-09-01 10:27:42 +0530

    com 3

--- expand.js
+++ expand.js
@@ -7,6 +7,8 @@ $('.expand-icon').on('click',function(){
 
     if(state==0){
         $('.nav-bar').css('max-height','200px');
+        $('.nav-bar').css('margin-bottom','20px');
+        $('.nav-bar').css('opacity','1');
         //$('.collapsible').css('max-height','200px');
         $('.expand-icon').attr('src','assets/close.png');
         state = 1;
@@ -15,6 +17,8 @@ $('.expand-icon').on('click',function(){
     
     if(state==1){
         $('.nav-bar').css('max-height','0px');
+        $('.nav-bar').css('margin-bottom','0px');
+        $('.nav-bar').css('opacity','0');
         //$('.collapsible').css('max-height','0px');
         $('.expand-icon').attr('src','assets/open-menu.png');
         state = 0;
